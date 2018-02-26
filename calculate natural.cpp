554442966291163abdad3131c757231a64bf57f5
praveenpravin111 <36357471+praveenpravin111@users.noreply.github.com>
2018-02-26 22:04:57 +0530

    calculate natural.cpp

---
+++ calculate natural.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i,n,sum=0;
+	cout<<"enter the number";
+	cin>>n;
+	for(i=0;i<=n;++i)
+	{
+	sum +=i;	
+	}
+	cout<<"sum="<<sum;
+	return 0;
+}
