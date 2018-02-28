86451989ec5684492afd055842507618540b045f
praveenpravin111 <36357471+praveenpravin111@users.noreply.github.com>
2018-02-28 17:52:24 +0530

    reverse a num.cpp

--- reverse a num.cpp
+++ reverse a num.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 int main()
 {
-int n,r,r1=0;
-cout<<"Enter n value:\n";
+int n,a,a1=0;
+cout<<"enter n value:\n";
 cin>>n;
 while(n!=0)
 {
-r=n%10;
-r1=r1*10+r;
+a=n%10;
+a1=a1*10+r;
 n=n/10;
 }
-cout<<"Reverse Number="<<r1;
+cout<<"\nreverse  Number is:"<<r1;
 return 0;
 }
