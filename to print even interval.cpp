1255c76897570a2e059b79ac5b9cbf2553480ec9
praveenpravin111 <36357471+praveenpravin111@users.noreply.github.com>
2018-03-01 17:29:48 +0530

    to print even interval.cpp

---
+++ to print even interval.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i,n;
+	cout<<"enter the number";
+	cin>>n;
+	for(i=1;i<=n;i++)
+	{
+	if(n%2==0)
+	{
+	   cout<<n;
+	}
+	}
+	return 0;
+}
