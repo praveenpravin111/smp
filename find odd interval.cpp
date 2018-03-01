51470f5029d27dd6c864915eec642be5a384af3e
praveenpravin111 <36357471+praveenpravin111@users.noreply.github.com>
2018-03-01 17:43:03 +0530

    find odd interval.cpp

---
+++ find odd interval.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int s,m;
+	cout<<"enter the numbers:";
+	cin>>s>>m;
+	for(int i=0;s<=m;i++)
+	{
+	s=s+2;
+	cout<<"\nthe interval is:";
+}
+return 0;
+}
