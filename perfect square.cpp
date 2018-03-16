7cdaaf7cad857b5f879884ec8731674ad3871aee
praveenpravin111 <36357471+praveenpravin111@users.noreply.github.com>
2018-03-16 17:52:30 +0530

    prefect square.cpp

---
+++ perfect square.cpp
@@ -0,0 +1,16 @@
+#include <iostream.h>
+using namespace std;
+ 
+bool isPerfectSquare(long double x)
+{  
+  long double sr = sqrt(x);
+  return ((sr - floor(sr)) == 0);
+}
+int main() {
+  long double x = 2500;
+  if (isPerfectSquare(x))
+    cout << "Yes";
+  else
+    cout << "No";
+  return 0;
+}
