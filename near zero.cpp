b3fd8af7d6a03a65c1949e93ae19f4f81cf75dd1
praveenpravin111 <36357471+praveenpravin111@users.noreply.github.com>
2018-03-20 17:30:16 +0530

    near aero.cpp

---
+++ near zero.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int num;
+	int i,j,a[n];
+	cout<<"enter number";
+	cin>>num;
+	for(i=0;i<=n;i++)
+	{
+		for(j=0;j<=num;j++)
+		{
+				if(a[i]+a[j]==0)
+					cout<<a[i]<<a[j];
+		}
+		}
+	return 0;
+}
