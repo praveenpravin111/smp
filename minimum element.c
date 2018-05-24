a9cade1adfc5d44c90c8d76c7373b97be6dd65fd
praveenpravin111 <36357471+praveenpravin111@users.noreply.github.com>
2018-05-24 11:56:01 +0530

    minimum element.c

---
+++ minimum element.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() 
+{
+    int array[50], min, size, c, location = 1;
+    printf("Enter the number of elements in array\n");
+    scanf("%d",&size);
+    printf("Enter %d integers\n", size);
+ 
+    for ( c = 0 ; c < size ; c++ )
+        scanf("%d", &array[c]);
+ 
+    min = array[0];
+ 
+    for ( c = 1 ; c < size ; c++ ) 
+    {
+        if ( array[c] < min ) 
+        {
+           min = array[c];
+           location = c+1;
+        }
+    } 
+ 
+    printf("Minimum element is present at location %d and it's value is %d.\n", location, min);
+    return 0;
+}
