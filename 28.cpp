fe579bb0e471a4c8a3b708f7b8e43eed0e0b8c38
praveenpravin111 <36357471+praveenpravin111@users.noreply.github.com>
2018-05-24 15:27:58 +0530

    28.cpp

---
+++ 28.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int i,n;
+cin>>n;
+int a[n];
+for(i=0;i<n;i++)
+{
+	
+cin>>a[i];
+}
+
+for(i=0;i<n;i++)
+{
+    cout<<i;
+    cout<<a[i]<<endl;
+}
+return 0;
+}
