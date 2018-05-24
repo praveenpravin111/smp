8c72f8f888887e43bee26e0621f798a73ad9a257
praveenpravin111 <36357471+praveenpravin111@users.noreply.github.com>
2018-05-24 16:18:11 +0530

    32.cpp

---
+++ 32.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+#include<stdlib.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    int i,c=1;
+    getline(cin, s);
+    for(i=0;i<s.length();i++)
+    {
+    if(s.at(i)==' ')
+    {
+        c++;
+    }
+}
+cout<<c;
+   return 0;
+}
