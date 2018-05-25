39637f8a85a1e5b22c263cc57e3b24acfca9196d
praveenpravin111 <36357471+praveenpravin111@users.noreply.github.com>
2018-05-25 14:06:54 +0530

    Create 37.cpp

---
+++ 37.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+void sawp(int x,int y)
+
+int main() {
+	int a=100,b=200;
+	cout<<"enter the value of  a:"<<a<<endl;
+	cout<<"enter the value of  b:"<<b<<endl;
+	swap(a,b)
+	cout<<"the value after swap a:"<<a<<endl;
+	cout<<"the value after swap b:"<<b<<endl;
+	return 0;
+}
