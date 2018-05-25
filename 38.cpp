d4747e71117bcc03a17f2ff9d70e09e82116407c
praveenpravin111 <36357471+praveenpravin111@users.noreply.github.com>
2018-05-25 14:09:57 +0530

    Create 38.cpp

---
+++ 38.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void swap(int &a,int &b)
+{
+  a=b^a;
+	b=a^b;
+	b=b^a;
+}
+int main() 
+{ 
+  int a,b;
+	cout<<"enter numbers";
+	cin>>a>>b;
+	swap(a,b);
+	cout<<a<<b;
+	return 0;
+}
