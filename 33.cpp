1b720c9ddd7d4fdaf17cd9c95037836eab81eacf
praveenpravin111 <36357471+praveenpravin111@users.noreply.github.com>
2018-05-25 12:07:21 +0530

    33.cpp

---
+++ 33.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+        string s;
+        int i,len,count=0;
+        cout<<"ENTER STRING: ";
+        getline(cin,s);
+        len=s.size();
+        for(i=0;i<len;i++)
+       {
+                if(s[i]==' ')
+                {
+                        count++;
+                }
+       }
+        cout<<"NUMBER OF SPACES IN THE GIVEN STRING ARE "<<count;
+        return 0;
+}
