7a2732baa31577b498abbbcbc36bb0f54b2fdedb
praveenpravin111 <36357471+praveenpravin111@users.noreply.github.com>
2018-05-25 14:15:48 +0530

    Create 39.cpp

---
+++ 39.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int i,n,num;
+  int larger=0;
+  cout<<"enter n"<<endl;
+	cin>>n;
+	for(i=0;i<=n;i++)
+	{
+		cin>>num;
+		if(num>large)
+		{
+			large=num;
+		}
+	}
+	cout<<large;
+	
+	return 0;
+}
